73ec21ea5e6a6c3be64dd0aab3f9a24cb39ae011
Chinmay-G <chinmay.the1@gmail.com>
2025-04-25 21:58:25 +0530

    Input section styling Done. DoNext: Style remaining all

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -3,90 +3,118 @@ import "../styles/InputSection.css";
 export default function InputSection() {
   return (
     <section className="input-section">
-      <div>
-        <div className="personal-details">
-          <h2>Personal Details</h2>
-          <div>⬇</div>
+      <div className="input-division personal-details">
+        <div style={{ width: "100%" }}>
+          <h2>
+            👤 Personal Details<button>🔽</button>
+          </h2>
+          {/* <div>⬇</div> */}
           <hr />
+        </div>
+        <div className="input-name">
+          <label>Full Name</label>
+          <input id="name" type="text" />
+        </div>
+        <div className="input-photo">
+          <label>Photo</label>
+          <label for="file-upload" class="custom-file-upload">
+            📸 Upload
+          </label>
+          <input id="file-upload" accept="image/*" type="file" name="photo" />
+        </div>
+        <div className="input-email">
+          <label>Email</label>
+          <input id="email" type="email" />
+        </div>
+        <div className="input-phone">
+          <label>Phone</label>
+          <input id="phone" type="text" />
+        </div>
+        <div>
+          <label>Address</label>
+          <input type="text" name="address" />
+        </div>
+        <div>
+          <label>Summary</label>
+          <textarea rows="3" />
+        </div>
+      </div>
+
+      <div className="input-division education">
+        <div>
+          <h2>Education</h2>
+
+          <hr />
+        </div>
+        <form>
+          <h3>BE Computer Science & Engineering</h3>
           <div>
-            <label>Full Name</label>
+            <label>Degree</label>
             <input type="text" />
           </div>
           <div>
-            <label>Photo</label>
-            <input type="file" accept="image/*" />
+            <label>School</label>
+            <input type="text" />
           </div>
-          <div>
-            <label>Email</label>
-            <input type="email" />
+          <div className="input-date">
+            <label>Start Date</label>
+            <input type="text" />
           </div>
-          <div>
-            <label>Address</label>
+          <div className="input-date">
+            <label>End Date</label>
             <input type="text" />
           </div>
           <div>
-            <label>Summary</label>
+            <label>Location</label>
+            <input type="text" />
+          </div>
+          <div className="mini-form-buttons">
+            <button className="delete-button">🗑️ Delete</button>
+            <button className="save-button">✔️ Save</button>
+            <button className="close-button">❌ Close</button>
+          </div>
+        </form>
+        <button className="add-mini-form-button">+ Education</button>
+      </div>
+
+      <div className="input-division experience">
+        <div>
+          <h2>Experience</h2>
+          <hr />
+        </div>
+        <form>
+          <h3>Designation</h3>
+          <div>
+            <label>Job Title</label>
+            <input type="text" placeholder="Jr. Developer | CEO | Teacher" />
+          </div>
+          <div>
+            <label>Company</label>
+            <input type="text" />
+          </div>
+          <div className="input-date">
+            <label>Start Date</label>
+            <input type="text" />
+          </div>
+          <div className="input-date">
+            <label>End Date</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Location</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Description</label>
             <textarea rows="3" />
           </div>
-        </div>
-
-        <div className="education">
-          <h2>Education</h2>
-          <form>
-            <h3>BE Computer Science & Engineering</h3>
-            <div>
-              <label>Degree</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>School</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Start Date</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>End Date</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Location</label>
-              <input type="text" />
-            </div>
-          </form>
-          <button>+ Education</button>
-        </div>
-
-        <div className="experience">
-          <h3>Designation</h3>
-          <form>
-            <div>
-              <label>Job Title</label>
-              <input type="text" placeholder="Jr. Developer | CEO | Teacher" />
-            </div>
-            <div>
-              <label>Company</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Start Date</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>End Date</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Location</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Description</label>
-              <textarea></textarea>
-            </div>
-          </form>
-        </div>
+          <div className="mini-form-buttons">
+            <button className="delete-button">🗑️ Delete</button>
+            <button className="save-button">✔️ Save</button>
+            <button className="close-button">❌ Close</button>
+          </div>
+        </form>
+        <button className="add-mini-form-button">+ Experience</button>
       </div>
     </section>
   );
