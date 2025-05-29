5a9f1540bebb8c7d64c5673c69f105daf45102e4
Chinmay-G <chinmay.the1@gmail.com>
2025-05-29 08:48:57 +0530

    Made responsive for all screen types

--- src/components/MiniFormButtons.jsx
+++ src/components/MiniFormButtons.jsx
@@ -7,7 +7,7 @@ export default function MiniFormButtons({ handleDelete, index, setIsOpen }) {
         <Trash2 size={18} /> Delete
       </button>
       <button className="done-button" onClick={() => setIsOpen((is) => !is)}>
-        <BookCheck size={18} /> Done/Close
+        <BookCheck size={18} /> Done
       </button>
     </div>
   );
